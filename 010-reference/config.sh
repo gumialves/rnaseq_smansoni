7da3486a8b2264ee77fa7889894d0eda0af39a11
ra236875 <ra236875@srv-Slurm-mgmt>
2025-08-23 18:29:57 -0300

    Inicial Struture project

---
+++ 010-reference/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Configurações gerais para referência
+
+# URL do genoma a ser baixado
+GENOME_URL="https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/schistosoma_mansoni/PRJEA36577/schistosoma_mansoni.PRJEA36577.WBPS19.genomic.fa.gz"
+
+GENOME_NAME="schistosoma_mansoni.PRJEA36577.WBPS19.genomic"
+
+OUT_DIR="reference"
+
+# para indexação
+THREADS=8
